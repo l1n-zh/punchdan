356144299ce9fdaf747a8da82ca730023c92493c
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-10-01 23:53:51 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+
+var count = 0;
+var record = 0;
+var normal = "./assets/normal.png";
+var punched = "./assets/punched.png";
+
+const checkBox = document.createElement("div");
+// checkBox.type = "checkbox";
+// checkBox.textContent = "0";
+const panel = document.getElementById('panel')
+alert(panel)
+panel.appendChild(checkBox);
+
+setInterval(() => {
+  if (count > 820)
+    normal = "./assets/red.png";
+  record = 0;
+}, 10000);
+
+function punch() {
+  ++count;
+  ++record;
+  const counter = document.getElementById('counter');
+  counter.style.rotate = `${(Math.random() - 0.5)/10}turn`;
+  counter.style.scale = "1.3";
+  counter.innerHTML = count;
+  document.getElementById('dan').src = punched;
+  const audio = document.createElement("audio");
+  audio.src = `./assets/sound/${Math.floor((Math.random()*11))}.mp3`;
+  audio.play();
+  setTimeout(() => {
+    counter.style.rotate = '0turn';
+    counter.style.scale = '1';
+  }, 50);
+}
+
+function release() {
+  document.getElementById('dan').src = normal;
+}
