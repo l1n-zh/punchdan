3c3d78e5105a8a9958ecb4ec6e67b480703e8582
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-10-02 00:18:59 +0800

    fix: 按鍵重複觸發

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 
 var count = 0;
 var record = 0;
-var normal = "./assets/normal.png";
-var punched = "./assets/punched.png";
+var normalImg = "./assets/normal.png";
 
+var punched = false;
+// window.onload = () => {
+//   const checkBox = document.createElement("div");
+//   checkBox.type = "checkbox";
+//   checkBox.textContent = "0";
+//   document.getElementById('panel').appendChild(checkBox);
+// }
 
 setInterval(() => {
   if (count > 820)
-    normal = "./assets/red.png";
+    normalImg = "./assets/red.png";
   record = 0;
 }, 10000);
 
 function punch() {
+  if (punched) return;
+  punched = true;
   ++count;
   ++record;
   const counter = document.getElementById('counter');
   counter.style.rotate = `${(Math.random() - 0.5)/10}turn`;
   counter.style.scale = "1.3";
   counter.innerHTML = count;
-  document.getElementById('dan').src = punched;
+  document.getElementById('dan').src = "./assets/punched.png";
   const audio = document.createElement("audio");
   audio.src = `./assets/sound/${Math.floor((Math.random()*11))}.mp3`;
   audio.play();
@@ -29,5 +37,6 @@ function punch() {
 }
 
 function release() {
-  document.getElementById('dan').src = normal;
+  punched = false;
+  document.getElementById('dan').src = normalImg;
 }
