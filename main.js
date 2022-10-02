83f77438649ff59aaf5787f1a2d57c670bdadd6b
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-10-02 16:48:22 +0800

    fix

--- main.js
+++ main.js
@@ -2,30 +2,27 @@ var count = 0;
 var normalImg = "./assets/normal.png";
 var punched = false;
 
-setInterval(() => {
-  if (count > 820)
-    normalImg = "./assets/red.png";
-}, 10000);
 
 function punch() {
   if (punched) return;
   punched = true;
   ++count;
-  const counter = document.getElementById('counter');
-  counter.style.rotate = `${(Math.random() - 0.5)/10}turn`;
+  if (count > 820) normalImg = "./assets/red.png";
+  const counter = document.getElementById("counter");
+  counter.style.rotate = `${(Math.random() - 0.5) / 10}turn`;
   counter.style.scale = "1.3";
   counter.innerHTML = count;
-  document.getElementById('dan').src = "./assets/punched.png";
+  document.getElementById("dan").src = "./assets/punched.png";
   const audio = document.createElement("audio");
-  audio.src = `./assets/sound/${Math.floor((Math.random()*11))}.mp3`;
+  audio.src = `./assets/sound/${Math.floor(Math.random() * 11)}.mp3`;
   audio.play();
   setTimeout(() => {
-    counter.style.rotate = '0turn';
-    counter.style.scale = '1';
+    counter.style.rotate = "0turn";
+    counter.style.scale = "1";
   }, 50);
 }
 
 function release() {
   punched = false;
-  document.getElementById('dan').src = normalImg;
+  document.getElementById("dan").src = normalImg;
 }
